b62ab4fec62756be9505675af9b1cd3188a34ef5
Karol Jasztal <jasztalkarol@gmail.com>
2026-09-02 15:09:08 +0200

    chore(migrations): renumber personal migrations to 9001/9002/9003 to avoid upstream number collisions

--- src/app/migrations/9002_itemmetadata.py
+++ src/app/migrations/9002_itemmetadata.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0062_animeavailability'),
+        ('app', '9001_animeavailability'),
     ]
 
     operations = [
--- src/app/migrations/9003_streaminglink.py
+++ src/app/migrations/9003_streaminglink.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0063_itemmetadata'),
+        ('app', '9002_itemmetadata'),
     ]
 
     operations = [
